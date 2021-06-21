3d626f2a64ab3b82a3c48c93162625167b98839b
Lebot Apps <lebot.llc@gmail.com>
2021-06-21 04:35:02 -0400

    added show to shesha

--- Models/shesha.js
+++ Models/shesha.js
@@ -5,7 +5,8 @@ const sheshaSchema = new Schema({
     name: {type: String, required: true},
     nicotine: {type: String, required: true},
     flavor: {type: String, required: true},
-    price: {type: Number, required: true}
+    price: {type: Number, required: true},
+    img: {type: String, required: true}
 });
 
 const Shesha = mongoose.model('Shesha', sheshaSchema);
