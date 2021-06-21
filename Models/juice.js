963e1367f13d4190dadc5a2c00c77f22c48c8720
Lebot Apps <lebot.llc@gmail.com>
2021-06-21 05:30:45 -0400

    added juice show

--- Models/juice.js
+++ Models/juice.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const juiceSchema = new Schema({
     name: {type: String, required: true},
-    description: {type: String, required: true},
     nicotine: {type: String, required: true},
     flavor: {type: String, required: true},
-    price: {type: Number, required: true}
+    price: {type: Number, required: true},
+    img: {type: String, required: true},
 });
 
 const Juice = mongoose.model('Juice', juiceSchema);
